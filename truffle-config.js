3966db0c5f2537bc039723a34db0814863b35d4b
mnava1418 <mnavapena@gmail.com>
2021-01-29 20:27:55 -0600

    initial setup

---
+++ truffle-config.js
@@ -0,0 +1,26 @@
+require('babel-register');
+require('babel-polyfill');
+require('dotenv').config();
+
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 8545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+    },
+  },
+
+  contracts_directory: './src/contracts/',
+  contracts_build_directory: './src/abis/',
+  
+  // Configure your compilers
+  compilers: {
+    solc: {
+      optimizer: {
+        enabled: false,
+        runs: 200
+      },
+    },
+  },
+};

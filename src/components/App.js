ab2a794943a16b78caa4ff6a977dce2258e91855
mnava1418 <mnavapena@gmail.com>
2021-02-11 20:25:34 -0600

    connect to blockchain and get Token

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,38 @@
-import React, { Component } from 'react'
+import React from 'react'
 import './style/App.css';
 import Web3 from 'web3';
+import Token from  '../abis/Token.json'
 
-class App extends Component {
+class App extends React.Component {
   componentWillMount() {
+    this.loadBlockChainData()
+  }
+  
+  async loadBlockChainData() {
+    const web3 = new Web3(Web3.givenProvider || 'http://localhost:8545')
+    const networkId = await web3.eth.net.getId()
+    const networks = Token.networks
+    const address = networks[networkId].address
+    const abi = Token.abi
+    const elberCoin = new web3.eth.Contract(abi, address)
 
+    const name = await elberCoin.methods.name().call() //call cuando solo quieres info. send es para generar una transaccion y cambiar algo en el block chain
+    const symbol = await elberCoin.methods.symbol().call()
+    
+    console.log('Name:', name )
+    console.log('Symbol:', symbol )
   }
 
   render(){
     return (
       <div>
           <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-            <a className="navbar-brand" href="/#">Navbar</a>
+            <a className="navbar-brand" href="/#">Elber Exchange</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNavDropdown">
               <ul className="navbar-nav">
-                <li className="nav-item">
-                  <a className="nav-link" href="/#">Link 1</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="/#">Link 2</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="/#">Link 3</a>
-                </li>
               </ul>
             </div>
           </nav>

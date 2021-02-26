2d4c220bd558214bbce603f1a3c7d578414502aa
mnava1418 <mnavapena@gmail.com>
2021-02-26 17:58:28 -0600

    add open orders selector

--- src/store/selectors/ordersSelector.js
+++ src/store/selectors/ordersSelector.js
@@ -3,6 +3,7 @@ import moment from 'moment'
 import { createSelector } from 'reselect'
 import { ETH_ADDRESS, fromWei} from '../../utils/ethUtil'
 
+//Filled Orders
 const filledOrdersLoaded = state => get(state, 'exchange.filledOrders.loaded', false)
 export const filledOrdersLoadedSelector = createSelector(filledOrdersLoaded, fl => fl)
 
@@ -16,6 +17,71 @@ export const filledOrdersSelector = createSelector(
         return orders
     }
 )
+
+//Canceled Orders
+const canceledOrdersLoaded = state => get(state, 'exchange.canceledOrders.loaded', false)
+export const canceledOrdersLoadedSelector = createSelector(canceledOrdersLoaded, cl => cl)
+
+const canceledOrders = state => get(state, 'exchange.canceledOrders.data', [])
+export const canceledOrdersSelector = createSelector(canceledOrders, orders => orders)
+
+//Open Orders
+const allOrdersLoaded = state => get(state, 'exchange.allOrders.loaded', false)
+export const allOrdersLoadedSelector = createSelector(
+    canceledOrdersLoaded,
+    filledOrdersLoaded,
+    allOrdersLoaded,
+    (cl, fl, al) => (cl && fl && al)
+)
+
+const allOrders = state => get(state, 'exchange.allOrders.data', [])
+export const openOrdersSelector = createSelector(
+    allOrders,
+    filledOrders,
+    canceledOrders,
+    (allOrders, filledOrders, canceledOrders) => {
+        const filledMap = getOrderMap(filledOrders)
+        const canceledMap = getOrderMap(canceledOrders)
+        
+        let openOrders = allOrders.filter( (order) => (!filledMap[order.id] && !canceledMap[order.id]) )
+        openOrders = openOrders.sort((a, b) => b.timeStamp - a.timeStamp)
+        openOrders = decorateOpenOrders(openOrders)
+        
+        const openOrdersByType = getOrdersByType(openOrders, ['Buy', 'Sell'])       
+        console.log('Open Orders: ', openOrdersByType)
+        return openOrdersByType
+    }
+)
+
+/*----
+HELPER FUNCTIONS
+----*/
+const getOrderMap = (orders = []) => {
+    let orderMap = {}
+    orders.forEach( (order) => {
+        orderMap[order.id] = order
+    } )
+
+    return orderMap
+}
+
+const getOrdersByType = (orders, types) => {
+    let ordersByType = {}
+    types.forEach( (type ) => {
+        ordersByType[type] = orders.filter( (order) => (order.orderType === type))
+    } )
+
+    return ordersByType
+}
+
+const decorateOpenOrders = (orders) => {
+    orders = orders.map( (order) => {
+        order = decorateOrder(order)
+        order = decorateOpenOrder(order)
+        return order
+    })
+    return orders
+}
 
 const decorateFilledOrders = (orders) => {
     let previousOrder = orders[0]
@@ -66,5 +132,24 @@ const decorateFilledOrder = (order, previousOrder) => {
     return ({
         ...order,
         tokenPriceClass
+    })
+}
+
+const decorateOpenOrder = (order) => {
+    let orderType
+    let orderTypeClass
+
+    if(order.tokenGive === ETH_ADDRESS) {
+        orderType = 'Buy'
+        orderTypeClass = 'success'
+    } else {
+        orderType = 'Sell'
+        orderTypeClass = 'danger'
+    }
+
+    return ({
+        ...order,
+        orderType,
+        orderTypeClass
     })
 }

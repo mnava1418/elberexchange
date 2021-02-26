3f612937cd0c197e4e2ba4b3ab3437fbb89fc4c4
mnava1418 <mnavapena@gmail.com>
2021-02-25 19:18:43 -0600

    remove collapse btn

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,9 +7,6 @@ class NavBar extends React.Component {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
             <a className="navbar-brand" href="/#">Elber Exchange</a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
             <ul className="navbar-nav ml-auto">
                 <li className= "nav-item">
                     <a

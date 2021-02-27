fcab6e9ee57ceef7114d09eb751ca61bde83738a
mnava1418 <mnavapena@gmail.com>
2021-02-26 18:22:27 -0600

    show order book

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Trades from './Trades'
+import OrderBook from './OrderBook'
 import { connect } from 'react-redux'
 import { exchangeSelector} from '../store/selectors/index'
 import { loadOrders } from '../store/interactions/exchangeInteractions'
@@ -36,17 +37,7 @@ class Content extends React.Component {
                 </div>
               </div>
             </div>
-            <div className="vertical">
-              <div className="card bg-dark text-white">
-                <div className="card-header">
-                  Card Title
-                </div>
-                <div className="card-body">
-                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                  <a href="/#" className="card-link">Card link</a>
-                </div>
-              </div>
-            </div>
+            <OrderBook />
             <div className="vertical-split">
               <div className="card bg-dark text-white">
                 <div className="card-header">
---
+++ src/components/OrderBook.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import Spinner from './Spinner'
+import { connect } from 'react-redux'
+import { allOrdersLoadedSelector, openOrdersSelector } from '../store/selectors/ordersSelector'
+
+const showOrders = (orders) => {
+  console.log(orders)
+  return(
+    <tbody>
+      {orders['Buy'].map((order) => {
+        return(
+          <tr className={`order-${order.id}`} key={order.id}>
+            <td>{order.tokenAmount}</td>
+            <td className={`text-${order.orderTypeClass}`}>{order.tokenPrice}</td>
+            <td>{order.ethAmount}</td>
+          </tr>
+        )
+      })}
+      <tr>
+        <th>ELB</th>
+        <th>ELB/ETH</th>
+        <th>ETH</th>
+      </tr>
+      {orders['Sell'].map((order) => {
+        return(
+          <tr className={`order-${order.id}`} key={order.id}>
+            <td>{order.tokenAmount}</td>
+            <td className={`text-${order.orderTypeClass}`}>{order.tokenPrice}</td>
+            <td>{order.ethAmount}</td>
+          </tr>
+        )
+      })}
+    </tbody>
+  )
+}
+
+class Content extends React.Component {
+  render(){
+    return (
+        <div className="vertical">
+            <div className="card bg-dark text-white">
+            <div className="card-header">
+                Order Book
+            </div>
+            <div className="card-body">
+                    <table className="table table-dark table-sm small">                        
+                        { this.props.ordersLoaded ? showOrders(this.props.openOrders) : <Spinner type="table"/> }
+                    </table>
+                </div>
+            </div>
+        </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+      ordersLoaded: allOrdersLoadedSelector(state),
+      openOrders: openOrdersSelector(state)
+  }
+}
+
+export default connect(mapStateToProps)(Content);

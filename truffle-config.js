752390b8c1e9a0f5ad68dc2d8c5c04e25af88222
mnava1418 <mnavapena@gmail.com>
2021-05-10 20:31:15 -0500

    migrate contracts to kovan

--- truffle-config.js
+++ truffle-config.js
@@ -1,6 +1,11 @@
 require('babel-register');
 require('babel-polyfill');
 require('dotenv').config();
+const HDWalletProvider = require('truffle-hdwallet-provider-privkey')
+
+const PRIVATE_KEYS = process.env.PRIVATE_KEYS
+const INFURA_KEY = process.env.INFURA_KEY
+
 
 module.exports = {
   networks: {
@@ -9,6 +14,17 @@ module.exports = {
       port: 8545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
     },
+    kovan: {
+      provider: function () {
+        return new HDWalletProvider(
+          PRIVATE_KEYS.split(','),
+          `https://kovan.infura.io/v3/${INFURA_KEY}`
+        )
+      },
+      gas: 5000000,
+      gasPrice: 25000000000,
+      network_id: 42
+    }
   },
 
   contracts_directory: './src/contracts/',

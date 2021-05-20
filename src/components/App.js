04d5314353ff3716c28b83d45a08cb57a00681fc
mnava1418 <mnavapena@gmail.com>
2021-05-19 21:02:03 -0500

    connect via metamask logic

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import './style/App.css';
 
 import NavBar from './NavBar'
 import Content from './Content'
+import Connect from './Connect'
 
 import { loadWeb3, loadAccount } from '../store/interactions/web3Interactions'
 import { loadToken } from '../store/interactions/tokenInteractions'
@@ -11,10 +12,40 @@ import { loadExchange } from '../store/interactions/exchangeInteractions'
 import { accountSelector, contractsLoadedSelector } from '../store/selectors/index'
 
 class App extends React.Component {
-  componentDidMount() {
-    this.loadBlockChainData(this.props.dispatch)
+  
+  constructor(props) {
+    super(props)
+    this.state = {
+      connectType: ''
+    }
   }
   
+  componentDidMount() {
+    this.detectMetamask()
+  }
+  
+  detectMetamask() {
+    if (window.ethereum) {
+      this.handleEthereum()
+    } else {
+      window.addEventListener('ethereum#initialized', this.handleEthereum(), {
+        once: true,
+      });
+
+      setTimeout(this.handleEthereum(), 3000);
+    }
+  }
+
+  handleEthereum() {
+    const { ethereum } = window;
+
+    if(ethereum && ethereum.isMetaMask) {
+      this.loadBlockChainData(this.props.dispatch)
+    } else {
+      this.setState((state) => ({...state, connectType: 'download'}))
+    }
+  }
+
   async loadBlockChainData(dispatch) {
     const web3 = loadWeb3(dispatch)
     const account = await loadAccount(web3, dispatch)
@@ -22,22 +53,22 @@ class App extends React.Component {
     const token = await loadToken(web3, dispatch)
     const exchange = await loadExchange(web3, dispatch)
 
+    if(!account) {
+      this.setState((state) => ({...state, connectType: 'connect'}))
+      return
+    } 
+
     if(!token || ! exchange) {
       window.alert('Contracts not found in current network. Please select another network via Metamask')
       return
-    }
-
-    if(!account) {
-      window.alert('Account not connected. Please connect account via Metamask')
-      return
-    } 
+    }   
   }
 
   render(){
     return (
         <div>
           <NavBar />
-          { (this.props.contractsLoaded && this.props.account) ? <Content /> : <div className="content"></div>}
+          { (this.props.contractsLoaded && this.props.account) ? <Content /> : <Connect type={this.state.connectType} /> }
         </div>
     );
   }
